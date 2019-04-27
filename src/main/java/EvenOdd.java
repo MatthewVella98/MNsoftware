d46215ef8bc64138299d60cf9b26e3437ca51379
neilvassallo <neil.vassallo.17@um.edu.mt>
2019-04-27 15:43:59 +0200

    Adding Jenkins Server Test 3: EvenOdd test

---
+++ src/main/java/EvenOdd.java
@@ -0,0 +1,16 @@
+public class EvenOdd {
+    public boolean isEven(int val){
+        if (val % 2 == 0){
+            return true;
+        }
+        else
+            return false;
+    }
+    public boolean isOdd(int val){
+        if (val % 2 == 0){
+            return false;
+        }
+        else
+            return true;
+    }
+}
