219b07ef1c3fb16dcc28b89d91dd283f104989dc
neilvassallo <vassball.nv@gmail.com>
2019-04-28 11:47:42 +0200

    Main game: Added base functionality on; Map, Player and Position.

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,11 +1,43 @@
+import java.util.InputMismatchException;
+
 public class Player {
-    private Position position;
+    private Position position; //Current position
+    private Position initPosition; //Initial position
 
-    public void move(char direction){
-
+    //Move enums: Can be passed as argument in the switch.
+    private enum Move{
+        LEFT,RIGHT,UP,DOWN
+    }
+    //Constructor
+    public Player(Position position){
+        this.position = position;
+        this.initPosition = position.HoldInitialPosition();
     }
 
-    public boolean setPosition(Position p){
-        return false;
+    //Move switch.
+    public void move(Move move){
+        switch(move){
+            case LEFT:
+                position.setX(position.getX() -1);
+                break;
+            case RIGHT:
+                position.setX(position.getX() +1);
+                break;
+            case UP:
+                position.setY(position.getY() +1);
+                break;
+            case DOWN:
+                position.setY(position.getY() -1);
+                break;
+            default:
+                throw new InputMismatchException("Invalid.");
+        }
     }
+
+    //Getters & Setters
+    public void setPosition(Position position){
+        this.position = position;
+    }
+
+    public Position getPosition() {return position; }
 }
--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,4 +1,46 @@
+import java.util.Random;
+
 public class Position {
     private int x;
     private int y;
+
+    //Constructors
+    public Position(){ }
+
+    public Position (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    //Getters & Setters
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    //Sets a random position and returns it.
+    public Position SetReturnPosition(int size){
+        Position position = new Position();
+
+        position.x = (int)(Math.random() * size) +1;
+        position.y = (int)(Math.random() * size) +1;
+
+        return position;
+    }
+
+    //Hold Initial Position
+    public Position HoldInitialPosition(){
+        return new Position(x,y);
+    }
 }
